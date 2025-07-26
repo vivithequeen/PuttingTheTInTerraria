e0c1e958dd0f0fb2a51eb1b71c97af0d21e9e27e
vivithequeen <vivithequeen1@gmail.com>
2025-07-26 11:32:24 -0400

    yay!

--- obj/Debug/net8.0/PuttingTheTInTerraria.AssemblyInfo.cs
+++ obj/Debug/net8.0/PuttingTheTInTerraria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuttingTheTInTerraria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbba386d49d4d37a84bb4d2ef70090d0279d2c01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8086072e90d1f10d5d0740b19d3b709afd85f20")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuttingTheTInTerraria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuttingTheTInTerraria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
