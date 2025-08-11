a7bc0412bef7fab1c872b4b1ac067c4063a6ea35
Bolb2019 <zachnoahstein@gmail.com>
2025-08-11 17:53:18 -0400

    First ship

--- obj/Debug/net8.0/PuttingTheTInTerraria.AssemblyInfo.cs
+++ obj/Debug/net8.0/PuttingTheTInTerraria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PuttingTheTInTerraria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c594ab66f6778ecd5b5b6b640eb7a9785e15fd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55368815614cb4f3f0e84f461e18e8a135584e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PuttingTheTInTerraria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PuttingTheTInTerraria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
